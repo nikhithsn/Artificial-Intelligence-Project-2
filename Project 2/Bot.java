d4a1eb361003bce0fc5fdbe65d691ab1f700af3b
nikhithsn <127979802+nikhithsn@users.noreply.github.com>
2024-07-21 12:23:20 -0400

    Update Bot.java

--- Project 2/Bot.java
+++ Project 2/Bot.java
@@ -1,33 +1,45 @@
 import java.util.Random;
 
-public abstract class Bot {
+public abstract class Bot 
+{
     protected int x, y;
     protected Grid grid;
     protected Random rand = new Random();
 
-    public Bot(Grid grid) {
+    public Bot(Grid grid) 
+    {
         this.grid = grid;
     }
 
-    public void setPosition(int x, int y) {
+    public void setPosition(int x, int y) 
+    {
         this.x = x;
         this.y = y;
     }
 
-    public void moveUp() {
+    public void moveUp() 
+    {
         if (x > 0) x--;
     }
 
-    public void moveDown() {
+    public void moveDown() 
+    {
         if (x < grid.getSize() - 1) x++;
     }
 
-    public void moveLeft() {
+    public void moveLeft() 
+    {
         if (y > 0) y--;
     }
 
-    public void moveRight() {
+    public void moveRight() 
+    {
         if (y < grid.getSize() - 1) y++;
+    }
+
+    public boolean hasFoundMouse()
+    {
+        return (grid.getCell(x, y).hasMouse());
     }
 
     public abstract void move();
